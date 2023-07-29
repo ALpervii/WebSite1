191e94db1ebc608d17ef23ea9bf8fdf958df0091
ALpervii <140815948+ALpervii@users.noreply.github.com>
2023-07-29 14:27:49 +0300

    ??????

--- WebSite2/Default.aspx.cs
+++ WebSite2/Default.aspx.cs
@@ -4,6 +4,9 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Web.UI.WebControls;
+using System.Data.Entity;
+using System.Runtime.InteropServices;
+using System.Web.Providers.Entities;
 
 public partial class _Default : System.Web.UI.Page
 {
@@ -16,56 +19,68 @@ public partial class _Default : System.Web.UI.Page
     }
     private void connection()
     {
-        string sqlconn = (@"server=.\SQLEXPRESS; Persist Security Info=true; Integrated Security=true; Initial Catalog=Excel");
+        //string sqlconn = (@"server=.\SQLEXPRESS; Persist Security Info=true; Integrated Security=true;  Initial Catalog=Excel;");
+        string sqlconn = (@"server = localhost; user = some_user; database = some_db; password = some_pass; charset = utf8;  Initial Catalog=Excel;");
         con = new SqlConnection(sqlconn);
-        //SqlConnection con = new SqlConnection(@"server=.\SQLEXPRESS; Persist Security Info=true; Integrated Security=true; Initial Catalog=Users");
+        //Коннект к базе данных
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        //Creating object of datatable  
+        //Создание обьекта базы данных  
         DataTable tblcsv = new DataTable();
-        //creating columns  
+        //Создание колонок  
         tblcsv.Columns.Add("id");
-        tblcsv.Columns.Add("name");
+        tblcsv.Columns.Add("DateCreate");
+        tblcsv.Columns.Add("DateEditing");
         tblcsv.Columns.Add("City");
+        tblcsv.Columns.Add("AdmDistrict");
+        tblcsv.Columns.Add("District");
+        tblcsv.Columns.Add("Street");
         tblcsv.Columns.Add("House");
-        //getting full file path of Uploaded file  
-        string CSVFilePath = Path.GetFullPath(@"C:\Users\123\Desktop\info.csv");
-        //Reading All text  
+        tblcsv.Columns.Add("Corps");
+        tblcsv.Columns.Add("YearBuilding");
+        //Положение файла  
+        string CSVFilePath = Path.GetFullPath(@"C:\Users\123\Desktop\ric_House.csv");
+        //string CSVFilePath = Path.GetFullPath(FileUpload1.PostedFile.FileName);
+        //Прочитать весь текст  
         string ReadCSV = File.ReadAllText(CSVFilePath);
-        //spliting row after new line  
+        //Сплит до новых строк  
         foreach (string csvRow in ReadCSV.Split('\n'))
         {
             if (!string.IsNullOrEmpty(csvRow))
             {
-                //Adding each row into datatable  
+                //добавление в колонки  
                 tblcsv.Rows.Add();
                 int count = 0;
-                foreach (string FileRec in csvRow.Split(','))
+                foreach (string FileRec in csvRow.Split(';'))
                 {
                     tblcsv.Rows[tblcsv.Rows.Count - 1][count] = FileRec;
                     count++;
                 }
             }
         }
-        //Calling insert Functions  
+         
         InsertCSVRecords(tblcsv);
     }
-    //Function to Insert Records  
+   
     private void InsertCSVRecords(DataTable csvdt)
     {
 
-        connection();
-        //creating object of SqlBulkCopy   
+        connection();  
         SqlBulkCopy objbulk = new SqlBulkCopy(con);
-        //assigning Destination table name   
+        //имя таблички   
         objbulk.DestinationTableName = "Users";
-        //Mapping Table column   
+        //Запись в колонки   
         objbulk.ColumnMappings.Add("id", "id");
-        objbulk.ColumnMappings.Add("name", "name");
+        objbulk.ColumnMappings.Add("DateCreate", "DateCreate");
+        objbulk.ColumnMappings.Add("DateEditing", "DateEditing");
         objbulk.ColumnMappings.Add("City", "City");
+        objbulk.ColumnMappings.Add("AdmDistrict", "AdmDistrict");
+        objbulk.ColumnMappings.Add("District", "District");
+        objbulk.ColumnMappings.Add("Street", "Street");
         objbulk.ColumnMappings.Add("House", "House");
-        //inserting Datatable Records to DataBase   
+        objbulk.ColumnMappings.Add("Corps", "Corps");
+        objbulk.ColumnMappings.Add("YearBuilding", "YearBuilding");
         con.Open();
         objbulk.WriteToServer(csvdt);
         con.Close();
